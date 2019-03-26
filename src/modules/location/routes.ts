326f7b297f9bd82ed275d032e9c6168361b3cf94
Paul Kariuki <paulrimiru@gmail.com>
2019-03-26 12:34:39 +0300

    chore(refactor): cleanup application

--- src/modules/location/routes.ts
+++ src/modules/location/routes.ts
@@ -13,22 +13,22 @@ const { BASE_URL } = process.env;
 
 export default [
   {
-    path: `${BASE_URL}/`,
+    path: `${BASE_URL}/location`,
     method: 'get',
     handler: getLocationHandler,
   },
   {
-    path: `${BASE_URL}/`,
+    path: `${BASE_URL}/location`,
     method: 'post',
     handler: postLocationHandler,
   },
   {
-    path: `${BASE_URL}/`,
+    path: `${BASE_URL}/location`,
     method: 'delete',
     handler: deleteLocationHandler,
   },
   {
-    path: `${BASE_URL}/`,
+    path: `${BASE_URL}/location`,
     method: 'put',
     handler: updateLocationHandler,
   },
